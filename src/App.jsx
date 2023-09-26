155d0897a93f6549ed013c61c80d764fef2600f8
Guna P <111977753+Guna-Pandi@users.noreply.github.com>
2023-09-26 20:51:07 +0530

    Added: Data as ojects

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Hero } from "./components";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,29 @@
+// imports
+
+const Hero = () => {
+  return (
+    <div>
+    {/* background */}
+        <div></div>
+        <div>
+        {/* text conten */}
+            <div>
+                <h1></h1>
+                <h1></h1>
+                <button></button>
+                {/* video links */}
+                <div></div>
+                {/* Solcial links */}
+                <div></div>
+            </div>
+            {/* image content */}
+            <div>
+                <img src="" alt="" className=" "/>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Hero
