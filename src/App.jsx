1172f75454a1f7f062bab45022f61085bd4ea536
Guna P <111977753+Guna-Pandi@users.noreply.github.com>
2023-10-01 23:22:22 +0530

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,13 @@
-import {
-  Cart,
-  FlexContent,
-  Footer,
-  Hero,
-  Navbar,
-  Sales,
-  Stories,
-} from "./components";
-import {
-  heroapi,
-  popularsales,
-  toprateslaes,
-  highlight,
-  sneaker,
-  story,
-  footerAPI,
-} from "./data/data.js";
+import React from 'react';
+import { Cart, FlexContent, Footer, Hero, Navbar, Sales, Stories } from './components';
+import { heroapi, popularsales, toprateslaes, highlight, sneaker, story, footerAPI } from './data/data.js';
 
-function App() {
+const App = () => {
   return (
-    <>
-      <Navbar />
-      <Cart/>
-      <main className=" flex flex-col gap-16 relative">
+   <>
+      <Navbar/>
+      <Cart />
+      <main className='flex flex-col gap-16 relative'>
         <Hero heroapi={heroapi} />
         <Sales endpoint={popularsales} ifExists />
         <FlexContent endpoint={highlight} ifExists />
@@ -31,8 +16,8 @@ function App() {
         <Stories story={story} />
       </main>
       <Footer footerAPI={footerAPI} />
-    </>
-  );
+   </>
+  )
 }
 
 export default App;
